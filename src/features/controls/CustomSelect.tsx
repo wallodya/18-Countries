f1782c810cb0d17ee5e5aead2d7004499ecbd6eb
wallodya <78960605+wallodya@users.noreply.github.com>
2022-12-13 21:30:54 +0400

    init

--- src/features/controls/CustomSelect.tsx
+++ src/features/controls/CustomSelect.tsx
@@ -1,58 +1,59 @@
-import styled from 'styled-components';
+import styled from "styled-components"
 
-import Select, { Props } from 'react-select';
+import Select, { Props } from "react-select"
+import { Region } from "types"
 
-export type CountryOption = {
-    label: Region
-    value: Region 
-}
+export type CountryOption =
+	| {
+			label: Region
+			value: Region
+	  }
+	| ""
 
 const MySelect = (props: Props<CountryOption, false>) => {
-    return (
-        <Select />
-    )
+	return <Select />
 }
 
 export const CustomSelect = styled(MySelect).attrs({
-  styles: {
-    control: (provided) => ({
-      ...provided,
-      backgroundColor: 'var(--colors-ui-base)',
-      color: 'var(--colors-text)',
-      borderRadius: 'var(--radii)',
-      padding: '0.25rem',
-      border: 'none',
-      boxShadow: 'var(--shadow)',
-      height: '50px',
-    }),
-    option: (provided, state) => ({
-      ...provided,
-      cursor: 'pointer',
-      color: 'var(--colors-text)',
-      backgroundColor: state.isSelected
-        ? 'var(--colors-bg)'
-        : 'var(--colors-ui-base)',
-    }),
-  },
+	styles: {
+		control: provided => ({
+			...provided,
+			backgroundColor: "var(--colors-ui-base)",
+			color: "var(--colors-text)",
+			borderRadius: "var(--radii)",
+			padding: "0.25rem",
+			border: "none",
+			boxShadow: "var(--shadow)",
+			height: "50px",
+		}),
+		option: (provided, state) => ({
+			...provided,
+			cursor: "pointer",
+			color: "var(--colors-text)",
+			backgroundColor: state.isSelected
+				? "var(--colors-bg)"
+				: "var(--colors-ui-base)",
+		}),
+	},
 })`
-  width: 200px;
-  border-radius: var(--radii);
-  font-family: var(--family);
-  border: none;
+	width: 200px;
+	border-radius: var(--radii);
+	font-family: var(--family);
+	border: none;
 
-  & > * {
-    box-shadow: var(--shadow);
-  }
+	& > * {
+		box-shadow: var(--shadow);
+	}
 
-  & input {
-    padding-left: 0.25rem;
-  }
+	& input {
+		padding-left: 0.25rem;
+	}
 
-  & * {
-    color: var(--colors-text) !important;
-  }
+	& * {
+		color: var(--colors-text) !important;
+	}
 
-  & > div[id] {
-    background-color: var(--colors-ui-base);
-  }
-`;
+	& > div[id] {
+		background-color: var(--colors-ui-base);
+	}
+`
--- src/features/controls/use-search.ts
+++ src/features/controls/use-search.ts
@@ -1,18 +1,18 @@
-import { ChangeEventHandler } from 'react';
-import {useSelector, useDispatch} from 'react-redux';
-import { useAppDispatch } from 'store';
-import { selectSearch } from './control-selectors';
-import { setSearch } from './controls-slice';
+import { ChangeEventHandler } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { useAppDispatch } from "store"
+import { selectSearch } from "./control-selectors"
+import { setSearch } from "./controls-slice"
 
 export type OnSearch = ChangeEventHandler<HTMLInputElement>
 
 export const useSearch = (): [string, OnSearch] => {
-  const dispatch = useAppDispatch();
-  const search = useSelector(selectSearch);
+	const dispatch = useAppDispatch()
+	const search = useSelector(selectSearch)
 
-  const handleSearch: OnSearch = (e) => {
-    dispatch(setSearch(e.target.value))
-  }
+	const handleSearch: OnSearch = e => {
+		dispatch(setSearch(e.target.value))
+	}
 
-  return [search, handleSearch];
+	return [search, handleSearch]
 }
